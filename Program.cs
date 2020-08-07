e3b4e491cf43b00b217e7cb6f0cf1c5b631691f7
chuck francis <chuck_francis10@yahoo.com>
2020-08-07 17:29:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08072020ValidatingInput
+{
+    class Program
+    {
+        // Demonstrate the need for validation of human input
+        // Parse vs TryParse
+        // looping to force valid input
+        static void Main(string[] args)
+        {
+
+            //= int.Parse(input);
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+
+                int number;
+
+                if (int.TryParse(input, out number))
+                {
+                    number++;
+                    Console.WriteLine("The number plus one is: {0}", number);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid number! ");
+                }
+            }
+        }
+    }
+}
